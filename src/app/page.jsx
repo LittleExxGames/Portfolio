164df81934345f6602e4bae6327018442cd7ab07
Joe Weilert <joey@dmhatch.com>
2024-04-03 18:38:30 +0000

    Filled Work Widget Content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,6 +43,27 @@ const Home = () => {
             startYear: 2016,
             endYear: null,
           },
+          {
+            logo: '/spotify.png',
+            organization: 'Spotify',
+            jobTitle: 'Software Engineer',
+            startYear: 2014,
+            endYear: 2015,
+          },
+          {
+            logo: '/audible.png',
+            organization: 'Audible',
+            jobTitle: 'Software Engineer',
+            startYear: 2012,
+            endYear: 2013,
+          },
+          {
+            logo: '/microsoft.png',
+            organization: 'Microsoft',
+            jobTitle: 'Software Engineer',
+            startYear: 2010,
+            endYear: 2011,
+          },
         ]}
       />
       </div>
