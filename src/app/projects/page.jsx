bbaadbeb137c8badf1ae7913dd96ddfacddb0860
ElijahLeBaron <155860564+ElijahLeBaron@users.noreply.github.com>
2024-04-08 17:42:11 +0000

    notes

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './page.css'; // Import CSS file for styling
+import './page.css';
 
 const CompanyList = () => {
-  // Define the CompanyCard component directly within the CompanyList component
   const CompanyCard = ({ description, website, logo, title }) => {
     return (
       <div className="company-card">
@@ -96,4 +95,41 @@ export default function Projects() {
   );
 
 }
+*/
+/*
+import './ProjectCard.css';
+import PropTypes from 'prop-types';
+
+const ProjectCard = ({ logo, name, content, link }) => {
+  return (
+    <div data-testid="projectCard" className="project-card">
+      <div>
+        <img data-testid="projectCardLogo" src={logo} alt={`${name} Logo`} className="project-card-logo" />
+        <h2 data-testid="projectCardName">{name}</h2>
+      </div>
+      <div>
+        <p data-testid="projectCardContent" className="project-card-content">
+          {content}
+        </p>
+        <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer" className="project-card-link">
+          <img src="/link.svg" alt="" className="project-card-link-img" />
+          View Project
+        </a>
+      </div>
+    </div>
+  );
+};
+
+ProjectCard.propTypes = {
+  logo: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  link: PropTypes.string,
+};
+
+ProjectCard.defaultProps = {
+  link: '#',
+};
+
+export default ProjectCard;
 */
