eccf771bb868090acd2c9b70ddaabce8f90dc3ae
ElijahLeBaron <155860564+ElijahLeBaron@users.noreply.github.com>
2024-04-08 19:53:33 +0000

    help

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,8 @@ import './page.css';
 import SignupWidget from '../components/SignupWidget/SignupWidget.jsx';
 import WorkWidget from '../components/WorkWidget/WorkWidget.jsx';
 import SkillsWidget from '../components/SkillsWidget/SkillsWidget.jsx';
-import Paragraph from '../components/Paragraph/Paragraph.js';
+import ArticleCard from '../components/ArticleCard/ArticleCard';
+import Paragraph from '../components/Paragraph/Paragraph.jsx';
 
 export const metadata = {
   title: 'Portfolio - Home',
@@ -29,57 +30,68 @@ const Home = () => {
     <div className='page'>
       <Paragraph title="Welcome to My Portfolio" text={["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]} />
       {/* Add more paragraphs as needed */}
-<div className='About-Me'>
+<div>
+          {/* Include the ArticleCard component with articleData */}
+          <ArticleCard
+            date={articleData.date}
+            title={articleData.title}
+            content={articleData.content}
+            link={articleData.link}
+          />
+        </div>
     <div>
-      <SignupWidget
-        title={'Stay up to date'}
-        content={'Get notified when I publish something new, and unsubscribe at any time.'}
-      />
+      <div className='About-Me'>
+        <div>
+          <SignupWidget
+            title={'Stay up to date'}
+            content={'Get notified when I publish something new, and unsubscribe at any time.'}
+          />
+        </div>
+        <div>
+          <WorkWidget
+            title={'Work'}
+            content={'My work experience.'}
+            experiences={[
+              {
+                logo: '/slack.png',
+                organization: 'Slack',
+                jobTitle: 'Software Engineer',
+                startYear: 2016,
+                endYear: null,
+              },
+              {
+                logo: '/spotify.png',
+                organization: 'Spotify',
+                jobTitle: 'Software Engineer',
+                startYear: 2014,
+                endYear: 2015,
+              },
+              {
+                logo: '/audible.png',
+                organization: 'Audible',
+                jobTitle: 'Software Engineer',
+                startYear: 2012,
+                endYear: 2013,
+              },
+              {
+                logo: '/microsoft.png',
+                organization: 'Microsoft',
+                jobTitle: 'Software Engineer',
+                startYear: 2010,
+                endYear: 2011,
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <SkillsWidget
+            title={'Skills'}
+            content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
+            skills={skills}
+          />
+        </div>
       </div>
-      <div>
-      <WorkWidget
-        title={'Work'}
-        content={'My work experience.'}
-        experiences={[
-          {
-            logo: '/slack.png',
-            organization: 'Slack',
-            jobTitle: 'Software Engineer',
-            startYear: 2016,
-            endYear: null,
-          },
-          {
-            logo: '/spotify.png',
-            organization: 'Spotify',
-            jobTitle: 'Software Engineer',
-            startYear: 2014,
-            endYear: 2015,
-          },
-          {
-            logo: '/audible.png',
-            organization: 'Audible',
-            jobTitle: 'Software Engineer',
-            startYear: 2012,
-            endYear: 2013,
-          },
-          {
-            logo: '/microsoft.png',
-            organization: 'Microsoft',
-            jobTitle: 'Software Engineer',
-            startYear: 2010,
-            endYear: 2011,
-          },
-        ]}
-      />
-      </div>
-      <div>
-        <SkillsWidget
-        title={'Skills'}
-        content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
-        skills={skills}
-      />
     </div>
-      </div>
     </div>
   );
 }
