db10e21104b6bcc84257f3d6a8a7c001eeab3c7f
John-Keitel <156012519+John-Keitel@users.noreply.github.com>
2024-04-08 18:11:22 +0000

    leveled paragraph and widgets

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
 import './page.css';
 import SignupWidget from '../components/SignupWidget/SignupWidget.jsx';
 import WorkWidget from '../components/WorkWidget/WorkWidget.jsx';
@@ -20,60 +19,56 @@ const skills = [
 const Home = () => {
   return (
     <div className='page'>
-      <Paragraph title="Welcome to My Portfolio" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
-      {/* Add more paragraphs as needed */}
-<div className='About-Me'>
-    <div>
-      <SignupWidget
-        title={'Stay up to date'}
-        content={'Get notified when I publish something new, and unsubscribe at any time.'}
-      />
+      <div className='content'>
+        <Paragraph title="Welcome to My Portfolio" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+        {/* Add more paragraphs as needed */}
       </div>
-      <div>
-      <WorkWidget
-        title={'Work'}
-        content={'My work experience.'}
-        experiences={[
-          {
-            logo: '/slack.png',
-            organization: 'Slack',
-            jobTitle: 'Software Engineer',
-            startYear: 2016,
-            endYear: null,
-          },
-          {
-            logo: '/spotify.png',
-            organization: 'Spotify',
-            jobTitle: 'Software Engineer',
-            startYear: 2014,
-            endYear: 2015,
-          },
-          {
-            logo: '/audible.png',
-            organization: 'Audible',
-            jobTitle: 'Software Engineer',
-            startYear: 2012,
-            endYear: 2013,
-          },
-          {
-            logo: '/microsoft.png',
-            organization: 'Microsoft',
-            jobTitle: 'Software Engineer',
-            startYear: 2010,
-            endYear: 2011,
-          },
-        ]}
-      />
-      </div>
-      <div>
+      <div className='About-Me'>
+        <SignupWidget
+          title={'Stay up to date'}
+          content={'Get notified when I publish something new, and unsubscribe at any time.'}
+        />
+        <WorkWidget
+          title={'Work'}
+          content={'My work experience.'}
+          experiences={[
+            {
+              logo: '/slack.png',
+              organization: 'Slack',
+              jobTitle: 'Software Engineer',
+              startYear: 2016,
+              endYear: null,
+            },
+            {
+              logo: '/spotify.png',
+              organization: 'Spotify',
+              jobTitle: 'Software Engineer',
+              startYear: 2014,
+              endYear: 2015,
+            },
+            {
+              logo: '/audible.png',
+              organization: 'Audible',
+              jobTitle: 'Software Engineer',
+              startYear: 2012,
+              endYear: 2013,
+            },
+            {
+              logo: '/microsoft.png',
+              organization: 'Microsoft',
+              jobTitle: 'Software Engineer',
+              startYear: 2010,
+              endYear: 2011,
+            },
+          ]}
+        />
         <SkillsWidget
-        title={'Skills'}
-        content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
-        skills={skills}
-      />
+          title={'Skills'}
+          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.'}
+          skills={skills}
+        />
+      </div>
     </div>
-      </div>
-      </div>
   );
 }
 
