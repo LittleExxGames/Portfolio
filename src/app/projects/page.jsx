02c3dd6d5fdb19934d883389d2ba7f4629e5f7dc
Bradley Braganca <131413946+BradleyBraganca@users.noreply.github.com>
2024-04-10 18:34:53 +0000

    Project Pages Adjustments

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -34,21 +34,21 @@ const CompanyList = () => {
         <CompanyCard
           description="Description of Company"
           website="https://www.example.com"
-          logo="/path/to/logo.png"
+          logo="ensign.png"
           title="Company Title"
           key="company1"
         />
         <CompanyCard
           description="Description of Company"
           website="https://www.example.com"
-          logo="/path/to/ensign.png"
+          logo="ensign.png"
           title="Company Title"
           key="company2"
         />
         <CompanyCard
           description="Description of Company"
           website="https://www.example.com"
-          logo="/path/to/logo.png"
+          logo="ensign.png"
           title="Company Title"
           key="company3"
         />
@@ -58,21 +58,21 @@ const CompanyList = () => {
         <CompanyCard
           description="Description of Company"
           website="https://www.example.com"
-          logo="/path/to/logo.png"
+          logo="ensign.png"
           title="Company Title"
           key="company4"
         />
         <CompanyCard
           description="Description of Company"
           website="https://www.example.com"
-          logo="/path/to/logo.png"
+          logo="ensign.png"
           title="Company Title"
           key="company5"
         />
         <CompanyCard
           description="Description of Company"
           website="https://www.example.com"
-          logo="/path/to/logo.png"
+          logo="ensign.png"
           title="Company Title"
           key="company6"
         />
@@ -82,54 +82,3 @@ const CompanyList = () => {
 };
 
 export default CompanyList;
-/*
-export const metadata = {
-  title: 'Portfolio - Projects',
-  description: 'The projects page of my portfolio.',
-}
-
-export default function Projects() {
-  return (
-    <div>Projects</div>
-
-  );
-
-}
-*/
-/*
-import './ProjectCard.css';
-import PropTypes from 'prop-types';
-
-const ProjectCard = ({ logo, name, content, link }) => {
-  return (
-    <div data-testid="projectCard" className="project-card">
-      <div>
-        <img data-testid="projectCardLogo" src={logo} alt={`${name} Logo`} className="project-card-logo" />
-        <h2 data-testid="projectCardName">{name}</h2>
-      </div>
-      <div>
-        <p data-testid="projectCardContent" className="project-card-content">
-          {content}
-        </p>
-        <a data-testid="projectCardLink" href={link} target="_blank" rel="noreferrer" className="project-card-link">
-          <img src="/link.svg" alt="" className="project-card-link-img" />
-          View Project
-        </a>
-      </div>
-    </div>
-  );
-};
-
-ProjectCard.propTypes = {
-  logo: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-  link: PropTypes.string,
-};
-
-ProjectCard.defaultProps = {
-  link: '#',
-};
-
-export default ProjectCard;
-*/
